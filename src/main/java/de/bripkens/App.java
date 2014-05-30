4336309cb2b861cadcb090ec121cdc6ee085ec4e
Ben Ripkens <bripkens.dev@gmail.com>
2014-05-30 13:46:58 +0200

    Add logging capabilities

--- src/main/java/de/bripkens/App.java
+++ src/main/java/de/bripkens/App.java
@@ -1,9 +1,14 @@
 package de.bripkens;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    LOGGER.debug("Hello World!");
   }
 
 }
