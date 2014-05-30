07f6591ba22a4bd1f3137c35732631115708b74b
Ben Ripkens <bripkens.dev@gmail.com>
2014-05-30 12:04:51 +0200

    Init

---
+++ src/main/java/de/bripkens/App.java
@@ -0,0 +1,9 @@
+package de.bripkens;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ src/test/java/de/bripkens/AppTest.java
@@ -0,0 +1,12 @@
+package de.bripkens;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() {
+    App.main(null);
+  }
+
+}
