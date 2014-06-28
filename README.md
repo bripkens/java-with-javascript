e46939dada14ad11d340c9c623b6f28888992adb
Ben Ripkens <bripkens.dev@gmail.com>
2014-06-28 17:28:06 +0200

    Link to the blog post and more

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Java 8 Nashorn Examples
 
+This repository contains some Java 8 Nashorn examples, tests and a small
+event loop polyfill. Many of the examples in this repository were the basis
+for my [enterJS talk](http://www.enterjs.de/abstracts#javascript-und-java-kombinieren-polyglotte-programmierung-auf-jvm).
+
+You can learn more about Java 8 and project Nashorn in my
+[upcoming article](https://blog.codecentric.de/en/2014/06/project-nashorn-javascript-jvm-polyglott/)
+about code sharing between JavaScript and Java.
+
 ## Setup
 
 ```
--- src/test/java/de/bripkens/nashorn/StrengthCalculatorWithoutEventLoopTest.java
+++ src/test/java/de/bripkens/nashorn/StrengthCalculatorWithoutEventLoopTest.java
@@ -28,6 +28,10 @@ public class StrengthCalculatorWithoutEventLoopTest extends AbstractNashornTest 
     // load dependency which registers global zxcvbn function
     engine.eval(slurp("/bower_components/zxcvbn/zxcvbn.js"));
 
+    // you should _never_ pass user provided values via string concatenation
+    // to Nashorn. Check out my other repository to see a better way of doing
+    // this with Nashorn:
+    // https://github.com/bripkens/nashorn-pw-check-example/blob/master/src/main/java/de/codecentric/StrengthChecker.java
     Map<String, Object> result;
     result = (Map<String, Object>) engine.eval("zxcvbn('myPasswordIsRatherLong');");
 
